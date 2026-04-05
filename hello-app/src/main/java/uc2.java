4593f20ac198f79718a21cc05081e23a6e60a510
ayush-syst <2389ayush@gmail.com>
2026-04-05 15:33:05 +0530

    updated uc2

--- hello-app/src/main/java/uc2.java
+++ hello-app/src/main/java/uc2.java
@@ -3,7 +3,7 @@ package main.java;
 public class uc2 {
     
     public static void main(String[] args) {
-        String name = args.length > 0 ? args[0] : "World";
+        String name = args[0];
         System.out.println("Hello, " + name + "!");
     }
 }
